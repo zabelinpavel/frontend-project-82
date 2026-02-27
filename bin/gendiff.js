a99a3f64330a6186ea1eb0cb9ef5c465795063b3
Pavel Zabelin <zabelin.pavel.work@gmail.com>
2026-02-27 15:48:04 +0100

    chore: setup CLI arguments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,4 +9,11 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0');
 
+program
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+
+program
+    .option('-f, --format <type>', 'output format');
+
 program.parse(process.argv);
